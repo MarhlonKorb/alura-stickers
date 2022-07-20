8004fd0488ea5b56bf4453fb8b81a60c57406e79
Marhlon Korb <marhlonkorb@hotmail.com>
2022-07-20 00:01:43 -0300

    Criação da classe InputStream
    Criação de uma classe responsável por armazenar imagem e formata-la de acordo com o tamanho e texto desejados. A imagem pode vir através da api que foi consumida na aula anterior.

--- src/App.java
+++ src/App.java
@@ -1,35 +1,44 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        //fazer uma conexão Http e buscar os top 250 filmes
-        String url = "https://api.mocki.io/v2/549a5d8b";
-
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
         URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = (HttpRequest) HttpRequest.newBuilder().uri(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        //System.out.print(body);
-
-        //extrair só os dados que interessam(título, poster, classificação)
-        JsonParser parser = new JsonParser();
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        //exibir e manipular dados
+        // exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String,String> filme : listaDeFilmes) {
 
-        for (Map<String, String> filme : listaDeFilmes
-             ) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
         }
     }
 }
